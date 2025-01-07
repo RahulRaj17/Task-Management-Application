aed48a22be866414586e2e7a2f186add16dc44ec
Rahul Raj <rahulraj.off@gmail.com>
2025-01-07 16:40:23 +0530

    Initialize Project

---
+++ task-management-app-service/src/main/java/com/rahul/task_management_app_service/TaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.task_management_app_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApplication.class, args);
+	}
+
+}
---
+++ task-management-app-service/src/test/java/com/rahul/task_management_app_service/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.task_management_app_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
